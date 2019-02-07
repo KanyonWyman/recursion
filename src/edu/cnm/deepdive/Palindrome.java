929bd2b6b4a158ee0df696f8a9b65b391883559c
kanyon <kanyonwyman@gmail.com>
2019-02-07 14:10:12 -0700

    added mcnugget and palindrome

---
+++ src/edu/cnm/deepdive/Palindrome.java
@@ -0,0 +1,15 @@
+package edu.cnm.deepdive;
+
+public class Palindrome {
+
+  private static boolean test(String candidate) {
+    String simpleCandidate = candidate.toLowerCase().replaceAll("[^a-z0-9]", "");
+    // TODO Assign simpleCandidate from candidate, stripping out spaces & punctuation.
+    return testSimple(simpleCandidate);
+  }
+
+  private static boolean testSimple(String candidate) {
+    return (candidate.length() <= 1) || (candidate.charAt(0) == candidate.charAt(candidate.length() - 1) && testSimple(candidate.substring(1, candidate.length() - 1)));
+  }
+
+}
